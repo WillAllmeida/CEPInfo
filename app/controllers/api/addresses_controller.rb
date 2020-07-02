080a9fd7dee9cf108bc7080e67e2dbb054c5a790
William Almeida <will.allmeida@gmail.com>
2020-07-02 03:58:23 -0300

    Add prevention of addition of the same cep many times

--- app/controllers/api/addresses_controller.rb
+++ app/controllers/api/addresses_controller.rb
@@ -15,27 +15,34 @@ module Api
             def create
                 address = Address.new(address_param)
 
-                uri = URI('http://cep.la/' + params[:cep])
+                addressCheck = Address.where(cep: params[:cep]).first
+                if !addressCheck
 
-                Net::HTTP.start(uri.host, uri.port) {|http|
-                    request = Net::HTTP::Get.new(uri)
-                    request["Accept"] = "application/json"
-                    response = http.request(request)
-                    
-                    
-                    @result = JSON.parse(response.body)
-                }
-                
-                address.city = @result["cidade"]
-                address.uf = @result["uf"]
-                address.address = @result["logradouro"]
-                address.district = @result["bairro"]
+                    uri = URI('http://cep.la/' + params[:cep])
 
-                if address.save
-                    render json: {status: 'SUCCESS', message:'Sucesso ao salvar informações do CEP', data:address},status: :ok
+                    Net::HTTP.start(uri.host, uri.port) {|http|
+                        request = Net::HTTP::Get.new(uri)
+                        request["Accept"] = "application/json"
+                        response = http.request(request)
+                        
+                        
+                        @result = JSON.parse(response.body)
+                    }
+
+                    address.city = @result["cidade"]
+                    address.uf = @result["uf"]
+                    address.address = @result["logradouro"]
+                    address.district = @result["bairro"]
+
+                    if address.save
+                        render json: {status: 'SUCCESS', message:'Sucesso ao salvar informações do CEP', data:address},status: :ok
+                    else
+                        render json: {status: 'ERROR', message:'Erro ao buscar informações do CEP informado', data:address.errors},status: :unprocessable_entity
+                    end
                 else
-                    render json: {status: 'ERROR', message:'Erro ao buscar informações do CEP informado', data:address.errors},status: :unprocessable_entity
+                    render json: {status: 'SUCCESS', message:'Sucesso ao retornar informações do CEP', data:addressCheck},status: :ok
                 end
+
             end
 		end
 end
